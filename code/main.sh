beab90e3b67d45bf4e04772bbc5c71d739d52770
Erez Amihud <erezamihud@gmail.com>
2024-10-16 20:19:23 +0300

    fix run

--- code/main.sh
+++ code/main.sh
@@ -29,12 +29,13 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
+set -e 
 for filename in $destDir/*; do
   echo Extracting $filename
   tempdir="name.$RANDOM.dir"
   mkdir $tempdir
   echo "Extracting $filename"
-  7z x $filename -o $tempdir
+  7z x $filename -o$tempdir
   ls $tempdir
   echo Creating manifest for $filename
   pdblister manifest $tempdir $manifestdir/$RANDOM
