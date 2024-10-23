e988c6cfe0df8ea0be42d56fa1edf600b41cc39c
Erez Amihud <erezamihud@gmail.com>
2024-10-23 03:36:25 +0300

    no more updates

--- code/daily.py
+++ code/daily.py
@@ -7,6 +7,7 @@ processed_builds = set(json.load(open("process_builds.json", "r")))
 builds = listid()
 builds = filter(lambda build: build.uuid not in processed_builds, builds)
 builds = filter(lambda build: build.arch in ["x86", "amd64"], builds)
+builds = filter(lambda build: "cumulative update " not in build.title.lower(), builds)
 
 builds = map(lambda build: build.uuid, builds)
 builds = list(builds)
