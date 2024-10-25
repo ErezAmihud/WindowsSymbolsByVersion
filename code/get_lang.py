42e55a99e51a7e8fc31ca9b7bd3ea026d8280ddf
Erez Amihud <erezamihud@gmail.com>
2024-10-25 18:08:45 +0300

    added better retry functinality

--- code/get_lang.py
+++ code/get_lang.py
@@ -1,3 +1,4 @@
+from functools import wraps
 import requests
 from pydantic import BaseModel
 from typing import List, Dict
@@ -10,6 +11,23 @@ class SchemaModel(BaseModel):
     response: ResponseModel
     jsonApiVersion: str
 
+def retry(times:int):
+    def inner_retry(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            count = times
+            while count > 0:
+                try:
+                    return func(*args, **kwargs)
+                except:
+                    count -= 1
+                    if count == 0:
+                        raise
+        return inner
+    return inner_retry
+
+
+@retry(4)
 def get_langs(uuid) -> List[str]:
     out = requests.get("https://api.uupdump.net/listlangs.php", params={"id": uuid}, allow_redirects=True)
     out.raise_for_status()
