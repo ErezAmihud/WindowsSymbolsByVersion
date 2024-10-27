f3e9a719ecf14f772d14140f8d0796e361c872d9
Erez Amihud <erezamihud@gmail.com>
2024-10-27 21:38:39 +0200

    remove the update filter

--- code/daily.py
+++ code/daily.py
@@ -12,7 +12,7 @@ processed_builds.union(set(json.load(open("problematic.json", "r"))))
 builds = listid()
 builds = filter(lambda build: build.uuid not in processed_builds, builds)
 builds = filter(lambda build: build.arch in ["x86", "amd64"], builds)
-builds = filter(lambda build: " update " not in build.title.lower(), builds) # filter updates from the db
+#builds = filter(lambda build: " update " not in build.title.lower(), builds) # filter updates from the db
 
 builds = filter(lambda build: "en-us" in get_langs(build.uuid), builds)
 actual_builds = []
