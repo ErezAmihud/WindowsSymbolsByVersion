718b1d3bad48485a8dc4d13e82563a61db7091b1
Erez Amihud <erezamihud@gmail.com>
2024-10-27 21:25:31 +0200

    improve logs on errors

--- code/daily.py
+++ code/daily.py
@@ -8,17 +8,19 @@ from get_lang import get_langs
 allowed_size=int(sys.argv[1])
 
 processed_builds = set(json.load(open("process_builds.json", "r")))
+processed_builds.union(set(json.load(open("problematic.json", "r"))))
 builds = listid()
 builds = filter(lambda build: build.uuid not in processed_builds, builds)
 builds = filter(lambda build: build.arch in ["x86", "amd64"], builds)
 builds = filter(lambda build: " update " not in build.title.lower(), builds) # filter updates from the db
 
-builds = map(lambda build: build.uuid, builds)
-builds = filter(lambda uuid: "en-us" in get_langs(uuid), builds)
+builds = filter(lambda build: "en-us" in get_langs(build.uuid), builds)
 actual_builds = []
 try:
     for i in range(allowed_size):
-        actual_builds.append(next(builds))
+        build = next(builds)
+        print(build.uuid, str(build))
+        actual_builds.append(build.uuid)
 except StopIteration:
     pass
 
